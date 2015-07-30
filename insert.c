bbe5afee19aa2589b68492ce40f8508e25a2b2f1
Thibaut Broggi <broggi_t@epitech.eu>
2015-07-30 16:40:56 +0200

    refactoring insert

--- insert.c
+++ insert.c
@@ -4,30 +4,16 @@
 struct s_node dummy = {0, 0, {&dummy, &dummy}};
 struct s_node *nil = &dummy;
 
-t_node *rotate_left(t_node **root) {
+t_node *rotate(t_node **root, int side) {
   t_node *old_root = *root;
-  t_node *new_root = (*root)->node[1];
+  t_node *new_root = (*root)->node[side];
 
   if ((*root = new_root) == nil)
     free(old_root);
   else {
-    old_root->node[1] = new_root->node[0];
+    old_root->node[side] = new_root->node[!side];
     old_root->height = 1 + MAX(old_root->node[0]->height, old_root->node[1]->height);
-    new_root->node[0] = old_root;
-  }
-  return new_root;
-}
-
-t_node *rotate_right(t_node **root) {
-  t_node *old_root = *root;
-  t_node *new_root = (*root)->node[0];
-
-  if ((*root = new_root) == nil)
-    free(old_root);
-  else {
-    old_root->node[0] = new_root->node[1];
-    old_root->height = 1 + MAX(old_root->node[0]->height, old_root->node[1]->height);
-    new_root->node[1] = old_root;
+    new_root->node[!side] = old_root;
   }
   return new_root;
 }
@@ -36,16 +22,10 @@ void balance(t_node **root) {
   int b = BALANCE(*root) / 2;
 
   if (b) {
-    if ((1 - b) / 2) {
-      if (BALANCE((*root)->node[1]) == -b)
-	rotate_right(&(*root)->node[1]);
-      *root = rotate_left(root);
-    }
-    else {
-      if (BALANCE((*root)->node[0]) == -b)
-	rotate_left(&(*root)->node[0]);
-      *root = rotate_right(root);
-    }
+    int const side = (1 - b) / 2;
+    if (BALANCE((*root)->node[side]) == -b)
+      rotate(&(*root)->node[side], !side);
+    *root = rotate(root, side);
   }
   if (*root != nil)
     (*root)->height = 1 + MAX((*root)->node[0]->height, (*root)->node[1]->height);
@@ -63,13 +43,7 @@ void insert(t_node **root, int val) {
     (*root)->node[1] = nil;
   }
   else if (val != (*root)->val) {
-    if (val > (*root)->val) {
-      insert(&(*root)->node[1], val);
-      balance(root);
-    }
-    else {
-      insert(&(*root)->node[0], val);
-      balance(root);
-    }
+    insert(&(*root)->node[val > (*root)->val], val);
+    balance(root);
   }
 }
